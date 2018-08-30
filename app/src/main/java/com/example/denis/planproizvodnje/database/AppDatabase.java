612c598adcd3527dbe0127b305d9323f833512dc
Denis <ka_bum@live.com>
2018-08-30 20:04:32 +0200

    Make DateConverter class to convert date(for and from database) including anotiations into AppDatapase as TypeConverters(of this class)

--- app/src/main/java/com/example/denis/planproizvodnje/database/AppDatabase.java
+++ app/src/main/java/com/example/denis/planproizvodnje/database/AppDatabase.java
@@ -3,11 +3,14 @@ package com.example.denis.planproizvodnje.database;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverter;
+import android.arch.persistence.room.TypeConverters;
 import android.content.Context;
 import android.util.Log;
 
 // Create database that uses TaskDao and Entity
 @Database(entities = {TaskEntry.class}, version = 1, exportSchema = false)
+@TypeConverters(DateConverter.class)
 public abstract class AppDatabase extends RoomDatabase{
 
     private static final String LOG_TAG = AppDatabase.class.getSimpleName();
